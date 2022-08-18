86414361854c1250df083de90a5402e2d2e430e4
Aron95 <peter@muertz.de>
2022-08-18 16:22:23 +0200

    0.8.3

--- Simulation/Assets/Scripts/MessageDotMovment.cs
+++ Simulation/Assets/Scripts/MessageDotMovment.cs
@@ -16,8 +16,8 @@ public class MessageDotMovment : MonoBehaviour
     {
         if (check)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 0.002f);
-            if((target.transform.position -transform.position).magnitude < 0.4f )
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 0.012f);
+            if((target.transform.position -transform.position).magnitude < 0.2f )
             {
                destroyObject();
             }
