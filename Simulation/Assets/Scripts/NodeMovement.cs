76762cbac0cf04b5eae2f80d8abf4d371252410a
Luis Dillmann <ldillmann@uni-koblenz.de>
2022-08-22 18:05:01 +0200

    0.8.6
    Added Neighbor List to waypoints and searching for new target waypoints in Pathfinding

--- Simulation/Assets/Scripts/NodeMovement.cs
+++ Simulation/Assets/Scripts/NodeMovement.cs
@@ -10,26 +10,51 @@ public class NodeMovement : MonoBehaviour
     public GameObject danger;
 
     public float angle;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    bool switchedTarget = false;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 safetyDir = Vector3.Normalize(gameObject.transform.position - danger.transform.position);
-        Debug.DrawRay(gameObject.transform.position, safetyDir);
+        Debug.DrawRay(gameObject.transform.position, safetyDir, Color.blue);
 
 
 		if (target != null)
 		{
-            Vector3 dirToTarget = target.transform.position - gameObject.transform.position;
-            Debug.DrawRay(gameObject.transform.position, dirToTarget);
-            angle = Vector3.Angle(safetyDir, dirToTarget);
-            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, speed);
+
+			if (target.transform.position == gameObject.transform.position && !switchedTarget)
+			{
+                List<GameObject> targetNeighbors = target.GetComponent<WaypointNeighbors>().neighbors;
+
+                GameObject newTarget = null;
+                float lowestAngle = 181;
+
+				foreach (GameObject neighbor in targetNeighbors) // finds neighbor that leads furthest away from danger
+				{
+                    Vector3 dirToNeighbor = neighbor.transform.position - gameObject.transform.position;
+                    float neighborAngle = Vector3.Angle(safetyDir, dirToNeighbor);
+
+                    if (neighborAngle < lowestAngle)
+					{
+                        newTarget = neighbor;
+                        lowestAngle = neighborAngle;
+					}
+				}
+
+                target = newTarget;
+
+
+                switchedTarget = true;
+			}
+			else
+			{
+                Vector3 dirToTarget = target.transform.position - gameObject.transform.position;
+                angle = Vector3.Angle(safetyDir, dirToTarget);
+                Debug.DrawRay(gameObject.transform.position, dirToTarget, Color.black);
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, speed);
+                switchedTarget = false;
+
+			}
 
 		}
     }
---
+++ Simulation/Assets/Scripts/WaypointNeighbors.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointNeighbors : MonoBehaviour
+{
+    public List<GameObject> neighbors = new List<GameObject>();
+}
