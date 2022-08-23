b73c0d4d93dd2b3cf1444c2050cd098c378c5b9b
Aron95 <peter@muertz.de>
2022-08-23 18:25:54 +0200

    add energy and spam protection

--- Simulation/Assets/Scripts/nodeProperty.cs
+++ Simulation/Assets/Scripts/nodeProperty.cs
@@ -1,41 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Timers;
+using System;
 // contains all information of a node and handles decision-making within node
 public class nodeProperty : MonoBehaviour
 {
     public int ip;
     public List<GameObject> nearNeighbour;
-    
+    public int energy = 100;
+    public int spamProtectionTimer = 10000;
+
+
+    //for spamfiltering
+    public int spamLimit = 5;
+    public List<int> blockedIps = new List<int>();
+
     // keeps track of which nodes alreay have message
     public Dictionary<messageContent, SortedSet<int>> messageTable = 
-        new Dictionary<messageContent, SortedSet<int>>(new MessageCompare());  
-    
-    
+        new Dictionary<messageContent, SortedSet<int>>(new MessageCompare());
+
+    public Dictionary<int, int> spamDetectionTable =
+     new Dictionary<int, int>();
+
+
+    //timer
+
+    private System.Timers.Timer aTimer;
+
     // gets a random IP from the main camera when Node is created / game is started
     void Start()
     {
         ip = Camera.main.GetComponent<ipProvider>().getRnd();
+        SetTimer();
     }
-  
+
+    private void SetTimer()
+    {
+        aTimer = new System.Timers.Timer(spamProtectionTimer);
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+    private void OnTimedEvent(object sender, ElapsedEventArgs e)
+    {
+        spamDetectionTable.Clear();
+    }
+
+    private void Update()
+    {
+       
+    }
+
 
     // handles storing incoming message
     public void addData(messageContent message)
     {
-        SortedSet<int> ips = new SortedSet<int>();
-        ips.Add(message.ip);
+        if(!(blockedIps.Contains(message.ip)))
+        {
 
-        if(!(messageTable.ContainsKey(message)))
-        {
-            messageTable.Add(message, ips);
-            routeMessage(message);
-        }
-        else
-        {
-            messageTable.TryGetValue(message, out ips);
+            //Spamprotection
+            if(message.ip != ip)
+                {
+                if (spamDetectionTable.ContainsKey(message.ip))
+                {
+                    int ipSpamCount;
+                    spamDetectionTable.TryGetValue(message.ip, out ipSpamCount);
+                    ipSpamCount++;
+                    spamDetectionTable.Remove(message.ip);
+                    spamDetectionTable.Add(message.ip, ipSpamCount);
+                    if (ipSpamCount >= spamLimit)
+                    {
+                        blockedIps.Add(message.ip);
+                        return;
+                    }
+                }
+                else
+                {
+                    spamDetectionTable.Add(message.ip, 1);
+                }
+            }
+
+            SortedSet<int> ips = new SortedSet<int>();
             ips.Add(message.ip);
-        }   
+
+            if (!(messageTable.ContainsKey(message)))
+            {
+                messageTable.Add(message, ips);
+                routeMessage(message);
+            }
+            else
+            {
+                messageTable.TryGetValue(message, out ips);
+                ips.Add(message.ip);
+            }
+        }
+        
     }
 
     // handles sending message to current neighbors
@@ -46,17 +107,22 @@ public class nodeProperty : MonoBehaviour
         SortedSet<int> ips;
         messageTable.TryGetValue(message,out ips);
 
-        foreach (GameObject neighbour in nearNeighbour)
+        if(energy >= 0)
         {
-            if(!(ips.Contains(neighbour.GetComponent<nodeProperty>().ip)))
+            foreach (GameObject neighbour in nearNeighbour)
             {
-                ips.Add(neighbour.GetComponent<nodeProperty>().ip);
-        
-                GetComponent<MessageShooter>().shootMessageDot(neighbour.GetComponent<Collider2D>(),message);
+                if (!(ips.Contains(neighbour.GetComponent<nodeProperty>().ip)))
+                {
+                    ips.Add(neighbour.GetComponent<nodeProperty>().ip);
+
+                    GetComponent<MessageShooter>().shootMessageDot(neighbour.GetComponent<Collider2D>(), message);
+                    energy -=1;
+                }
             }
+            Debug.Log("routeMessage" + nearNeighbour);
         }
-        Debug.Log("routeMessage"+nearNeighbour);
     }
+  
 
 
     public void printDict()
